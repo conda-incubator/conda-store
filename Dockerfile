f20a01a7ef1cf5e1832a97aeaa6f8e171573e50f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-07-16 09:09:59 -0400

    Adding docker registry and detailed readme

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM continuumio/anaconda3:2020.02
+FROM condaforge/miniforge3:latest
+
+COPY environment.yaml /opt/conda-store/environment.yaml
+
+RUN conda env create -f /opt/conda-store/environment.yaml
 
 COPY conda_store /opt/conda-store/conda_store
 
--- conda_store/registry.py
+++ conda_store/registry.py
@@ -1,32 +1,37 @@
 import json
 
+from flask import Flask, Response
 import requests
-from flask import Flask, Response, redirect, g
-
-from conda_store.data_model.base import DatabaseManager
 
 
-def docker_token(image):
+def _docker_token(image):
     url = f'https://auth.docker.io/token?service=registry.docker.io&scope=repository:{image}:pull'
     response = requests.get(url)
     return response.json()['token']
 
 
-def docker_request_tags(image):
-    token = docker_token(image)
+def _docker_request_tags(image):
+    token = _docker_token(image)
     url = f'https://index.docker.io/v2/{image}/tags/list'
     return requests.get(url, headers={'Authorization': f'Bearer {token}'}).json()
 
 
-def docker_request_mainfest(image, tag):
-    token = docker_token(image)
+def _docker_request_manifest(image, tag):
+    token = _docker_token(image)
     url = f'https://index.docker.io/v2/{image}/manifests/{tag}'
-    return requests.get(url, headers={'Authorization': f'Bearer {token}'}).json()
+    response = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+    return response.json()
 
 
-def json_response(data, status=200, mimetype='application/json'):
-    # doesn't like newlines in json strings
-    response = Response(json.dumps(data).replace(r'\n', ''), status=status, mimetype=mimetype)
+def _download_blob(image, blob):
+    token = _docker_token(image)
+    url = f'https://index.docker.io/v2/{image}/blobs/{blob}'
+    response = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+    return response.content
+
+
+def _json_response(data, status=200, mimetype='application/json'):
+    response = Response(json.dumps(data, indent=3), status=status, mimetype=mimetype)
     response.headers['Docker-Distribution-Api-Version'] = 'registry/2.0'
     return response
 
@@ -34,38 +39,25 @@ def json_response(data, status=200, mimetype='application/json'):
 def start_registry_server(conda_store, address='0.0.0.0', port=5001):
     app = Flask(__name__)
 
-    def get_dbm(conda_store):
-        dbm = getattr(g, '_dbm', None)
-        if dbm is None:
-            dbm = g._dbm = DatabaseManager(conda_store)
-        return dbm
-
-    @app.teardown_appcontext
-    def close_connection(exception):
-        dbm = getattr(g, '_dbm', None)
-        if dbm is not None:
-            dbm.close()
-
     @app.route("/v2/")
     def v2():
-        return json_response({})
+        return _json_response({})
 
     @app.route("/v2/<path:rest>")
     def list_tags(rest):
         parts = rest.split('/')
         if len(parts) > 2 and parts[-2:] == ['tags', 'list']:
             image = '/'.join(parts[:-2])
-            return json_response(docker_request_tags(image))
+            return _json_response(_docker_request_tags(image))
         elif len(parts) > 2 and parts[-2] == 'manifests':
             image = '/'.join(parts[:-2])
             tag = parts[-1]
-            return json_response(docker_request_mainfest(image, tag), mimetype='application/vnd.docker.distribution.manifest.v1+prettyjws')
+            return _json_response(_docker_request_manifest(image, tag), mimetype='application/json')
         elif len(parts) > 2 and parts[-2] == 'blobs':
             image = '/'.join(parts[:-2])
             blob = parts[-1]
-            url = 'https://index.docker.io/v2/{image}/blobs/{blob}'
-            return redirect(url)
+            return (_download_blob(image, blob), 200, {'Docker-Distribution-Api-Version': 'registry/2.0'})
         else:
-            return json_response({'status': 'error', 'path': rest}, status=404)
+            return _json_response({'status': 'error', 'path': rest}, status=404)
 
     app.run(debug=True, host=address, port=port)
