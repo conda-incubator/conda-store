7dce95e525333e901d0e4b12824d44322f5f69c5
anirrudh <anik597@gmail.com>
2021-02-01 23:27:02 -0500

    add rest of editor content

---
+++ extension/src/interfaces.ts
@@ -0,0 +1,19 @@
+export interface IEnv {
+  /* The name of the environment */
+  name: string;
+  /* The build number of the environment (essentially, the revision number) */
+  build_id: number;
+  /* The size of the environment in bytes */
+  size: number;
+  /* The sha-256 specification of the build. */
+  specification: string;
+  /* The path to the store */
+  store_path: string;
+  /* OPT/TODO: Jupyter Kerenel JSON Parse metadata */
+  jupyter_kernel?: string;
+  /* OPT: A string that describes the env*/
+  conda_desc?: string;
+  /* OPT: A single string to describe the environment's name in jupyter */
+  jupyter_desc?: string;
+}
+
