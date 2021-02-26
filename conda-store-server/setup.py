389364c3f5af69659babca1d4b265323dc15a032
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-02-26 10:06:45 -0500

    Bump version 0.2.2

--- conda-store-server/setup.py
+++ conda-store-server/setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='conda-store-server',
-    version='0.2.1',
+    version='0.2.2',
     description='Conda Environment Management, Builds, and Serve',
     long_description=long_description,
     long_description_content_type='text/markdown',
