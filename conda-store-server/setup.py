906290947b4a488cb2c2a9f9d94953fb16fd059f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-02-26 10:00:54 -0500

    Bump version 0.2.1

--- conda-store-server/setup.py
+++ conda-store-server/setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='conda-store-server',
-    version='0.2.0',
+    version='0.2.1',
     description='Conda Environment Management, Builds, and Serve',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- conda-store/setup.py
+++ conda-store/setup.py
@@ -40,7 +40,7 @@ with open("README.md", "r") as fh:
 
 setup_args = dict(
     name="conda_store",
-    version='0.2.0',
+    version='0.2.1',
     url="https://github.com/Quansight/conda-store",
     author="Anirrudh Krishnan",
     description="A JupterLab Extension and client to interface with conda-store",
