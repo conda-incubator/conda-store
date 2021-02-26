380cd3a192cb28af7b093b130f9d61f182a5454f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-02-26 09:34:34 -0500

    Cleanup setup.py

--- conda-store/setup.py
+++ conda-store/setup.py
@@ -1,4 +1,3 @@
-import json
 import os
 from jupyter_packaging import (
         create_cmdclass, install_npm, ensure_targets,
@@ -7,64 +6,47 @@ from jupyter_packaging import (
 import setuptools
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-name="conda_store"
-lab_path = os.path.join(HERE, name, "labextension")
+LAB_PATH = os.path.join(HERE, "conda_store", "labextension")
 
-jstargets = [
-    os.path.join(lab_path, "package.json"),
-]
-
-package_data_spec = {
-    name: [
-        "*"
+cmdclass = create_cmdclass(
+    "jsdeps",
+    package_data_spec={
+        "conda_store": [
+            "*"
+        ]
+    },
+    data_files_spec=[
+        ("share/jupyter/labextensions/%s" % "@Quansight/conda-store", LAB_PATH, "**"),
+        ("share/jupyter/labextensions/%s" % "@Quansight/conda-store", HERE, "install.json"),
     ]
-}
-
-jstargets = [
-    os.path.join(lab_path, "package.json"),
-]
-
-package_data_spec = {
-    name: [
-        "*"
-    ]
-}
-
-labext_name = "@Quansight/conda-store"
-
-data_files_spec = [
-    ("share/jupyter/labextensions/%s" % labext_name, lab_path, "**"),
-    ("share/jupyter/labextensions/%s" % labext_name, HERE, "install.json"),
-]
-
-cmdclass = create_cmdclass("jsdeps",
-    package_data_spec=package_data_spec,
-    data_files_spec=data_files_spec
 )
 
 js_command = combine_commands(
     install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
-    ensure_targets(jstargets),
+    ensure_targets([
+        os.path.join(LAB_PATH, "package.json"),
+    ]),
 )
 
-is_repo = os.path.exists(os.path.join(HERE, ".git"))
-if is_repo:
+if os.path.exists(os.path.join(HERE, ".git")):
     cmdclass["jsdeps"] = js_command
 else:
-    cmdclass["jsdeps"] = skip_if_exists(jstargets, js_command)
+    cmdclass["jsdeps"] = skip_if_exists([
+        os.path.join(LAB_PATH, "package.json"),
+    ], js_command)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup_args = dict(
-    name=name,
-    version='0.0.1',
+    name="conda_store",
+    version='0.2.0',
     url="https://github.com/Quansight/conda-store",
     author="Anirrudh Krishnan",
-    description="A JupterLab Extension to interface with conda-store",
-    long_description= long_description,
+    description="A JupterLab Extension and client to interface with conda-store",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    cmdclass= cmdclass,
+    cmdclass=cmdclass,
     packages=setuptools.find_packages(),
     install_requires=[
         "jupyterlab>=3.0.0",
