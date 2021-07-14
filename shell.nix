b5f12342cd4532be19d4b672d23a927d904d796f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-07-14 09:23:21 -0400

    Updating black version

--- shell.nix
+++ shell.nix
@@ -6,30 +6,5 @@ in
 pkgs.mkShell {
   buildInputs = [
     pkgs.docker-compose
-    pkgs.kubectl
-    pkgs.minikube
-    pkgs.vagrant
-    pkgs.ansible
-
-    pythonPackages.pyyaml
-    pythonPackages.flask
-    pythonPackages.flask-cors
-    pythonPackages.requests
-    pythonPackages.minio
-    pythonPackages.sqlalchemy
-    pythonPackages.minio
-    pythonPackages.pydantic
-
-    pythonPackages.pytest
-    pythonPackages.pytest-mock
-    pythonPackages.black
-    pythonPackages.flake8
-    pythonPackages.sphinx
-    pythonPackages.recommonmark
-    pythonPackages.sphinx_rtd_theme
   ];
-
-  shellHook = ''
-    export PATH="$PWD/node_modules/.bin/:$PATH"
-  '';
 }
