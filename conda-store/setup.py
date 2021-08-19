39803d7ab7eca11fb43397635aae8debeeaac6bc
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-08-19 17:38:23 -0400

    Removing no longer needed jupyterlab extension

--- conda-store/setup.py
+++ conda-store/setup.py
@@ -1,62 +1,24 @@
-import os
-from jupyter_packaging import (
-        create_cmdclass, install_npm, ensure_targets,
-        combine_commands, skip_if_exists
-        )
-import setuptools
+from setuptools import setup, find_packages
+import pathlib
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-LAB_PATH = os.path.join(HERE, "conda_store", "labextension")
+here = pathlib.Path(__file__).parent.resolve()
 
-cmdclass = create_cmdclass(
-    "jsdeps",
-    package_data_spec={
-        "conda_store": [
-            "*"
-        ]
-    },
-    data_files_spec=[
-        ("share/jupyter/labextensions/%s" % "@Quansight/conda-store", LAB_PATH, "**"),
-        ("share/jupyter/labextensions/%s" % "@Quansight/conda-store", HERE, "install.json"),
-    ]
-)
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
-js_command = combine_commands(
-    install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
-    ensure_targets([
-        os.path.join(LAB_PATH, "package.json"),
-    ]),
-)
-
-if os.path.exists(os.path.join(HERE, ".git")):
-    cmdclass["jsdeps"] = js_command
-else:
-    cmdclass["jsdeps"] = skip_if_exists([
-        os.path.join(LAB_PATH, "package.json"),
-    ], js_command)
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup_args = dict(
-    name="conda_store",
+setup(
+    name="conda-store",
     version='0.2.5',
     url="https://github.com/Quansight/conda-store",
-    author="Anirrudh Krishnan",
-    description="A JupterLab Extension and client to interface with conda-store",
+    author="Chris Ostrouchov",
+    description="A client to interface with conda-store",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    cmdclass=cmdclass,
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "jupyterlab>=3.0.0",
-    ],
-    zip_safe=False,
-    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[],
     python_requires=">=3.6",
     license="BSD-3-Clause",
     platforms="Linux, Mac OS X, Windows",
-    keywords=["Jupyter", "JupyterLab", "JupyterLab3", "conda-store"],
+    keywords=["conda-store"],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -74,7 +36,3 @@ setup_args = dict(
         "Source": "https://github.com/quansight/conda-store",
     },
 )
-
-
-if __name__ == "__main__":
-    setuptools.setup(**setup_args)
