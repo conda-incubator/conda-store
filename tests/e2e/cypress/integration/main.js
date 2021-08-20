9199fcbe10c42233172f94a26e17a238cd025e55
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-08-20 14:55:02 -0400

    Adding new prefix path to tests

--- tests/e2e/cypress/integration/main.js
+++ tests/e2e/cypress/integration/main.js
@@ -1,11 +1,19 @@
 describe('First Test', () => {
   it('Check conda-store home page', () => {
-    cy.visit('/');
+    cy.visit('/conda-store/');
 
-    cy.visit('/login/')
+    cy.visit('/conda-store/login/')
 
     cy.get('#login > a')
       .should('contain', 'Sign in with JupyterHub')
       .click();
+
+    cy.get('#username_input')
+      .type('conda-store-test');
+
+    cy.get('#password_input')
+      .type('test');
+
+    cy.get('form').submit();
   })
 })
