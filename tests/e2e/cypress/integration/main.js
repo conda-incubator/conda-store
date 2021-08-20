ae292dfd9af4cddfbebcb0ce374eba521f48b007
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-08-20 17:41:30 -0400

    Extending tests

--- tests/e2e/cypress/integration/main.js
+++ tests/e2e/cypress/integration/main.js
@@ -19,6 +19,30 @@ describe('First Test', () => {
     cy.get('#password_input')
       .type('test');
 
+    // for some reason this does not
+    // respect redirect_uri
     cy.get('form').submit();
+
+    // visit login page
+    cy.visit('/conda-store/login/')
+
+    // click on sign in with jupyterhub
+    // login through jupyterhub oauth server
+    cy.get('#login > a')
+      .should('contain', 'Sign in with JupyterHub')
+      .click();
+
+    // jupyterhub authorize access
+    cy.get('form > input').click()
+    cy.url().should('include', 'user')
+
+    // visit home page again
+    cy.get('a.navbar-brand').click()
+
+    // visit environment
+    cy.get('h5.card-title > a').contains('filesystem/python-numpy-env').click()
+
+    // visit build
+    cy.get('li.list-group-item > a').contains('Build').click()
   })
 })
