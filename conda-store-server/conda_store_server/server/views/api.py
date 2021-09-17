76de6bdfed73e91bdf2368c6145732575bdcd169
Pierre Simonard <pierre.olivier.simonard@gmail.com>
2021-09-17 14:35:56 +0200

    code formatted with black

--- conda-store-server/conda_store_server/server/views/api.py
+++ conda-store-server/conda_store_server/server/views/api.py
@@ -26,8 +26,8 @@ def get_sorted_args(request, allowed_sorts=[]):
         sort_by = None
 
     order = request.args.get("order", "asc")
-    if order not in ['asc', 'desc']:
-        order = 'asc'
+    if order not in ["asc", "desc"]:
+        order = "asc"
 
     return sort_by, order
 
@@ -71,10 +71,12 @@ def api_list_environments():
 
     limit, offset = get_paginated_args(request)
 
-    sort_by, order = get_sorted_args(request, allowed_sorts=['namespace', 'name'])
-    
+    sort_by, order = get_sorted_args(request, allowed_sorts=["namespace", "name"])
+
     orm_environments = auth.filter_environments(
-        api.list_environments(conda_store.db, search=search, sort_by=sort_by, order=order)
+        api.list_environments(
+            conda_store.db, search=search, sort_by=sort_by, order=order
+        )
     )
     return paginated_api_response(
         orm_environments, schema.Environment, limit, offset, exclude={"current_build"}
