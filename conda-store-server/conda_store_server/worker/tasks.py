7117f49cf90c711b0829bd11f356ecaf513a428e
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-09-17 17:33:14 -0400

    Didn't look like it fixed circular dependency

--- conda-store-server/conda_store_server/worker/tasks.py
+++ conda-store-server/conda_store_server/worker/tasks.py
@@ -156,6 +156,5 @@ def task_delete_environment(self, environment_id):
         ).all():
             delete_build_artifact(conda_store, build_artifact)
         conda_store.db.delete(build)
-        conda_store.db.commit()
     conda_store.db.delete(environment)
     conda_store.db.commit()
