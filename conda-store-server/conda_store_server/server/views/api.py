98e3987792634729871f9f53a5552c846d69bc52
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-09-22 23:03:29 -0400

    jsonify the response

--- conda-store-server/conda_store_server/server/views/api.py
+++ conda-store-server/conda_store_server/server/views/api.py
@@ -81,13 +81,13 @@ def paginated_api_response(
     count = query.count()
     query = query.order_by(*sorts).limit(limit).offset(offset)
 
-    return {
+    return jsonify({
         "status": "ok",
         "data": [object_schema.from_orm(_).dict(exclude=exclude) for _ in query.all()],
         "page": (offset // limit) + 1,
         "size": limit,
         "count": count,
-    }
+    })
 
 
 @app_api.route("/api/v1/")
