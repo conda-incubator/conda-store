3294cec192d65703025eaff3d20a601bce00a64a
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-02-14 20:29:35 -0500

    Updating nix dependencies

--- flake.nix
+++ flake.nix
@@ -7,9 +7,25 @@
 
   outputs = inputs@{ self, nixpkgs, ... }: {
     devShell.x86_64-linux =
-      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+        pythonPackages = pkgs.python3Packages;
       in pkgs.mkShell {
-        buildInputs = [ pkgs.docker-compose ];
+        buildInputs = [
+          pkgs.docker-compose
+
+          pythonPackages.yarl
+          pythonPackages.requests
+          pythonPackages.pydantic
+          pythonPackages.pyyaml
+          pythonPackages.traitlets
+          pythonPackages.celery
+          pythonPackages.flask
+          pythonPackages.flask-cors
+          pythonPackages.pyjwt
+          pythonPackages.minio
+        ];
       };
   };
 }
