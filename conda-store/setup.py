15b21f94b81524b726a78b5fccca4614c5b34452
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-03-24 10:48:42 -0400

    Bump version to 0.3.14

--- conda-store/setup.py
+++ conda-store/setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="conda-store",
-    version='0.3.13',
+    version='0.3.14',
     url="https://github.com/Quansight/conda-store",
     author="Chris Ostrouchov",
     description="A client to interface with conda-store",
