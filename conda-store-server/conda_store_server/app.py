0b4328c7f98678a235e1effb88c8b00b7c4d6265
Pierre Simonard <pierre.olivier.simonard@gmail.com>
2022-07-30 10:12:37 +0200

    API impact to update an env description

--- conda-store-server/conda_store_server/app.py
+++ conda-store-server/conda_store_server/app.py
@@ -518,6 +518,18 @@ class CondaStore(LoggingConfigurable):
 
         tasks.task_update_environment_build.si(environment.id).apply_async()
 
+    def update_environment_description(self, namespace, name, description):
+        
+        environment = api.get_environment(self.db, namespace=namespace, name=name)
+        if environment is None:
+            raise utils.CondaStoreError(
+                f"environment namespace={namespace} name={name} does not exist"
+            )
+
+        environment.description = description
+        self.db.commit()
+
+
     def delete_namespace(self, namespace):
         namespace = api.get_namespace(self.db, name=namespace)
         if namespace is None:
