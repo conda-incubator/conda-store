f80012d311774001a21f7797d8524563a79c8414
anirrudh <anirrudh@protonmail.com>
2023-02-09 16:21:52 -0500

    add test for workers

--- tests/test_multiple_workers.py
+++ tests/test_multiple_workers.py
@@ -5,28 +5,55 @@ same namespace on the server, checking to see if file-locking really works.
 
 This will use the exposed api as opposed to running playwright style.
 """
+import json
 import pathlib
-
+import uuid
 
 import yaml
 
+from conda_store_server import schema
+
 ENVIRONMENT_YAML_PATH = pathlib.Path("./assets/environments/python-parallel-test.yaml")
+
 
 def read_environment_spec(name: str, p: pathlib.Path = ENVIRONMENT_YAML_PATH):
     with open(p, "r") as f:
         spec = yaml.safe_load(f)
 
     spec["name"] = name
-    return spec
+    return json.dumps(spec)
 
-def test_build_a(testclient):
-    read_environment_spec("test_build_a")
-    pass
 
-def test_build_b(testclient):
-    read_environment_spec("test_build_b")
-    pass
+def test_parallel_specification_build_auth(testclient):
+    namespace = "default"
+    envs = {}
+    env_a = f"pytest-{uuid.uuid4()}"
+    env_b = f"pytest-{uuid.uuid4()}"
 
-def test_build_c(testclient):
-    read_environment_spec("test_build_c")
-    pass
+    testclient.login()
+
+    for e in (env_a, env_b):
+        envs[e] = {"namespace": namespace, "specification": read_environment_spec(e)}
+
+        response = testclient.post("api/v1/specification", envs[e])
+        response.raise_for_status()
+
+        r = schema.APIPostSpecification.parse_obj(response.json())
+        assert r.status == schema.APIStatus.OK
+        envs[e] = r
+
+    for e in envs:
+        # check for the given build
+        response = testclient.get(f"api/v1/build/{envs[e].data.build_id}")
+        response.raise_for_status()
+
+        r = schema.APIGetBuild.parse_obj(response.json())
+        assert r.status == schema.APIStatus.OK
+        assert r.data.specification.name == e
+
+        # check for the given environment
+        response = testclient.get(f"api/v1/environment/{namespace}/{e}")
+        response.raise_for_status()
+
+        r = schema.APIGetEnvironment.parse_obj(response.json())
+        assert r.data.namespace.name == namespace
