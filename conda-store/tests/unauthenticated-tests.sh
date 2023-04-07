d2908b803a6a58b6fe66deabae68ca273a857994
Christopher Ostrouchov <chris.ostrouchov@gmail.com>
2023-04-06 22:23:20 -0400

    Switch to use hatch for conda-store (#449)

---
+++ conda-store/tests/unauthenticated-tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export CONDA_STORE_URL=http://localhost:5000/conda-store
+export CONDA_STORE_AUTH=none
+
+conda-store --version
+conda-store info
+conda-store token
+conda-store list namespace
+conda-store list build
+conda-store list environment
