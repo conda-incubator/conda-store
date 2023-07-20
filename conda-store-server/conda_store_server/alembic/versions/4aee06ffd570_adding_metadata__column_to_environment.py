30bb143af98c1b4b06285cf65619a246cf0967ff
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2023-07-20 07:56:59 -0400

    Adding initial metadata schema migration

---
+++ conda-store-server/conda_store_server/alembic/versions/4aee06ffd570_adding_metadata__column_to_environment.py
@@ -0,0 +1,24 @@
+"""adding metadata_ column to environment
+
+Revision ID: 4aee06ffd570
+Revises: b387747ca9b7
+Create Date: 2023-07-19 17:09:43.180420
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4aee06ffd570'
+down_revision = 'b387747ca9b7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
