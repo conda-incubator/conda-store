5db256d0abd089920a6bd585dedc471d28ffd016
Pavithra Eswaramoorthy <pavithraes@outlook.com>
2023-08-11 01:51:56 +0530

    🔧 Update names for review-ability

---
+++ docusaurus-docs/docusaurus.config.js
@@ -0,0 +1,131 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'conda-store',
+  tagline: 'Data science environments, for collaboration',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://conda.store',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // // GitHub pages deployment config.
+  // // If you aren't using GitHub pages, you don't need these.
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/conda-incubator/conda-store/tree/main/docs',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'conda-store',
+        logo: {
+          alt: 'conda-store logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'right',
+            label: 'Tutorial',
+          },
+          {
+            href: 'https://github.com/conda-incubator/conda-store',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Documentation',
+            items: [
+              {
+                label: 'TODO',
+                to: '/',
+              },
+            ],
+          },
+          {
+            title: 'Repositories',
+            items: [
+              {
+                label: 'conda-store',
+                href: 'https://github.com/conda-incubator/conda-store',
+              },
+              {
+                label: 'conda-store-ui',
+                href: 'https://github.com/conda-incubator/conda-store-ui',
+              },
+              {
+                label: 'jupyterlab-conda-store',
+                href: 'https://github.com/conda-incubator/jupyterlab-conda-store',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'TODO',
+                to: '/',
+              },
+              {
+                label: 'TODO',
+                href: '/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} | Made with 💚 by conda-store dev team`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ docusaurus-docs/src/components/HomepageFeatures/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: '🧶 Flexible',
+    description: (
+      <>
+        Create and update environments quickly using a graphical UI or a YAML editor.
+        All environments are automatically version-controlled, and available for use.
+      </>
+    ),
+  },
+  {
+    title: '📋 Reproducible',
+    description: (
+      <>
+        Share environments effortlessly through the auto-generated artifacts: lockfile, docker image, YAML file, or tarball.
+        Exact versions of all packages and their dependencies are pinned in these artifacts.
+      </>
+    ),
+  },
+  {
+    title: '⚖️ Governance',
+    description: (
+      <>
+        Access admin-approved packages and channels, and request new ones when needed.
+        Admins have role-based access management, to allow users to share environments across (and only with) their team.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
