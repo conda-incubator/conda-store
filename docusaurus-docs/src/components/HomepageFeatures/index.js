9afcc68eb938d0c04c7e07dbb638422ebace5854
Pavithra Eswaramoorthy <pavithraes@outlook.com>
2023-08-11 02:51:40 +0530

    🎨 Homepage design

--- docusaurus-docs/src/components/HomepageFeatures/index.js
+++ docusaurus-docs/src/components/HomepageFeatures/index.js
@@ -36,7 +36,7 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
+        <h2>{title}</h2>
         <p>{description}</p>
       </div>
     </div>
--- docusaurus-docs/src/pages/index.js
+++ docusaurus-docs/src/pages/index.js
@@ -4,20 +4,24 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import CondaStoreLogo from '@site/static/img/logo.svg';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--secondary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <CondaStoreLogo
+          alt={siteConfig.title}
+          className={clsx(styles.logo, 'margin-vert--md')}
+        />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--primary button--lg"
             to="/">
             Get Started
           </Link>
@@ -31,7 +35,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
