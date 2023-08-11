ced3ca9c004d4262cdc13861a3acbbbb86992a6e
Pavithra Eswaramoorthy <pavithraes@outlook.com>
2023-08-11 05:49:32 +0530

    🔧 Structure fixes

--- docusaurus-docs/docs/explanations/index.md
+++ docusaurus-docs/docs/explanations/index.md
@@ -4,3 +4,8 @@ description: Conceptual guides
 ---
 
 # Conceptual guides
+
+import DocCardList from '@theme/DocCardList';
+import {useCurrentSidebarCategory} from '@docusaurus/theme-common';
+
+<DocCardList items={useCurrentSidebarCategory().items}/>
--- docusaurus-docs/src/pages/index.js
+++ docusaurus-docs/src/pages/index.js
@@ -23,7 +23,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/">
+            to="/docs/get-started/">
             Get Started
           </Link>
         </div>
