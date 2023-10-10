1afb0fc0f5abfa9931a856098bfb61d5adc00df8
Aaron Meurer <asmeurer@gmail.com>
2023-10-10 15:42:09 -0600

    Add an option to not redirect stderr in context.run
    This is needed for uses with --json, because otherwise any stderr breaks the
    json.

--- conda-store-server/conda_store_server/action/base.py
+++ conda-store-server/conda_store_server/action/base.py
@@ -18,7 +18,7 @@ def action(f: typing.Callable):
             # redirect stdout -> action_context.stdout
             stack.enter_context(contextlib.redirect_stdout(action_context.stdout))
 
-            # redirect stderr -> action_context.stdout
+            # redirect stderr -> action_context.stderr
             stack.enter_context(contextlib.redirect_stderr(action_context.stdout))
 
             # create a temporary directory
@@ -38,6 +38,7 @@ class ActionContext:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.stdout = io.StringIO()
+        self.stderr = io.StringIO()
         self.log = logging.getLogger(f"conda_store_server.action.{self.id}")
         self.log.propagate = False
         self.log.addHandler(logging.StreamHandler(stream=self.stdout))
@@ -45,13 +46,15 @@ class ActionContext:
         self.result = None
         self.artifacts = {}
 
-    def run(self, *args, **kwargs):
+    def run(self, *args, redirect_stderr=True, **kwargs):
         result = subprocess.run(
             *args,
             **kwargs,
             stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
+            stderr=subprocess.STDOUT if redirect_stderr else subprocess.PIPE,
             encoding="utf-8",
         )
         self.stdout.write(result.stdout)
+        if not redirect_stderr:
+            self.stderr.write(result.stderr)
         return result
