169bef7d97ecbe1fa7e9c4033ec1fe383f3339c4
Nikita Karetnikov <nikita@karetnikov.org>
2023-11-25 20:57:43 +0100

    Update migration

--- conda-store-server/conda_store_server/alembic/versions/0e213092db14_add_v2_role_mappings.py
+++ conda-store-server/conda_store_server/alembic/versions/0e213092db14_add_v2_role_mappings.py
@@ -1,16 +1,16 @@
 """add v2 role mappings
 
-Revision ID: 37c342536b84
-Revises: b387747ca9b7
-Create Date: 2023-10-29 21:32:01.560843
+Revision ID: 0e213092db14
+Revises: d78e9889566a
+Create Date: 2023-11-25 20:52:55.641442
 
 """
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "37c342536b84"
-down_revision = "b387747ca9b7"
+revision = "0e213092db14"
+down_revision = "d78e9889566a"
 branch_labels = None
 depends_on = None
 
