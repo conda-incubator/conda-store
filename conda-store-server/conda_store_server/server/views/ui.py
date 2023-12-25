478e7058ffc695c76614bd3f8515182a79cb85ac
Nikita Karetnikov <nikita@karetnikov.org>
2023-12-25 07:05:21 +0100

    Use `sys.platform` directly

--- conda-store-server/conda_store_server/server/views/ui.py
+++ conda-store-server/conda_store_server/server/views/ui.py
@@ -64,7 +64,7 @@ async def ui_list_environments(
             "environments": orm_environments.all(),
             "registry_external_url": server.registry_external_url,
             "entity": entity,
-            "platform": "Windows" if sys.platform == "win32" else "Linux, macOS",
+            "platform": sys.platform,
         }
 
         return templates.TemplateResponse("home.html", context)
@@ -210,7 +210,7 @@ async def ui_get_build(
             "registry_external_url": server.registry_external_url,
             "entity": entity,
             "spec": yaml.dump(build.specification.spec),
-            "platform": "Windows" if sys.platform == "win32" else "Linux, macOS",
+            "platform": sys.platform,
         }
 
         return templates.TemplateResponse("build.html", context)
