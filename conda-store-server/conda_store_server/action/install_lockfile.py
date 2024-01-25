f5642680d1365124e24c7d870d67bba891e9e57e
Nikita Karetnikov <nikita@karetnikov.org>
2024-01-25 19:26:35 +0100

    Improve wording

--- conda-store-server/conda_store_server/action/install_lockfile.py
+++ conda-store-server/conda_store_server/action/install_lockfile.py
@@ -18,9 +18,9 @@ def action_install_lockfile(
     with lockfile_filename.open("w") as f:
         json.dump(conda_lock_spec, f)
 
-    # This essentially disables caching by having a dedicated cache per
-    # environment. This is necessary because concurrent jobs override packages
-    # in the shared cache, which leads to failed builds.
+    # This essentially disables caching by having a dedicated cache per build.
+    # This is necessary because concurrent jobs override packages in the shared
+    # cache, which leads to failed builds.
     #
     # For example, packages A and B use the same dependency. Package A starts
     # the download process and puts the dependency in the cache. Package B sees
@@ -30,10 +30,10 @@ def action_install_lockfile(
     # package B fails.
     #
     # This is a known problem in conda and I'm not aware of any solution besides
-    # just disabling the cache by having a per-environment temporary cache
-    # directory. The name of the temporary directory is returned from this
-    # action so that the directory can be deleted once the package is done
-    # building, as that requires having access to dependencies from the cache.
+    # just disabling the cache by having a per-build temporary cache directory.
+    # The name of the temporary directory is returned from this action so that
+    # the directory can be deleted once the package is done building, as that
+    # requires having access to dependencies from the cache.
     #
     # Here are the relevant issues from the upstream repo, but they were closed
     # without any solution:
