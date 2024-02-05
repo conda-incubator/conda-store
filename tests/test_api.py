c49015ba85c800146ca73fc04e67bde2838fc9a0
Nikita Karetnikov <nikita@karetnikov.org>
2024-02-05 22:06:54 +0100

    Increase the test timeout again

--- tests/test_api.py
+++ tests/test_api.py
@@ -661,8 +661,8 @@ def test_create_specification_auth_env_name_too_long(testclient, size):
 
     # Try checking that the status is 'FAILED'
     is_updated = False
-    for _ in range(10):
-        time.sleep(5)
+    for _ in range(60):
+        time.sleep(10)
 
         # check for the given build
         response = testclient.get(f"api/v1/build/{build_id}")
