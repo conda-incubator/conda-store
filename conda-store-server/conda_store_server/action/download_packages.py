c60823aa1d10e03cdfd0f2af637abfcb7291ebfc
Nikita Karetnikov <nikita@karetnikov.org>
2024-02-05 01:05:51 +0100

    Fix the caching bug

--- conda-store-server/conda_store_server/action/download_packages.py
+++ conda-store-server/conda_store_server/action/download_packages.py
@@ -1,10 +1,21 @@
+import os
 import pathlib
 import shutil
 import typing
 
+# This import is needed to avoid the following error on conda imports:
+# AttributeError: 'Logger' object has no attribute 'trace'
+import conda.gateways.logging  # noqa
 import conda_package_handling.api
 import conda_package_streaming.url
 import filelock
+from conda.core.package_cache_data import (
+    PackageCacheRecord,
+    PackageRecord,
+    getsize,
+    read_index_json,
+    write_as_json_to_file,
+)
 from conda_store_server import action, conda_utils
 
 
@@ -28,6 +39,7 @@ def action_fetch_and_extract_conda_packages(
             file_path = pkgs_dir / filename
             count_message = f"{packages_searched} of {total_packages}"
             with filelock.FileLock(str(lock_filename)):
+                file_path_str = str(file_path)
                 if file_path.exists():
                     context.log.info(f"SKIPPING {filename} | FILE EXISTS\n")
                 else:
@@ -38,4 +50,35 @@ def action_fetch_and_extract_conda_packages(
                     ) = conda_package_streaming.url.conda_reader_for_url(url)
                     with file_path.open("wb") as f:
                         shutil.copyfileobj(conda_package_stream, f)
-                    conda_package_handling.api.extract(str(file_path))
+                    conda_package_handling.api.extract(file_path_str)
+
+                    # Otherwise .conda, do nothing in that case
+                    ext = ".tar.bz2"
+                    if file_path_str.endswith(ext):
+                        extracted_dir = pathlib.Path(file_path_str.replace(ext, ""))
+                        # This file is used to parse cache records via PackageCacheRecord in conda
+                        repodata_file = extracted_dir / "info" / "repodata_record.json"
+                        index_file = extracted_dir / "info" / "index.json"
+                        assert index_file.exists()
+
+                        if not repodata_file.exists():
+                            raw_json_record = read_index_json(extracted_dir)
+                            fn = os.path.basename(file_path_str)
+                            md5 = package["hash"]["md5"]
+                            size = getsize(file_path_str)
+
+                            package_cache_record = PackageCacheRecord.from_objects(
+                                raw_json_record,
+                                url=url,
+                                fn=fn,
+                                md5=md5,
+                                size=size,
+                                package_tarball_full_path=file_path_str,
+                                extracted_package_dir=str(extracted_dir),
+                            )
+
+                            repodata_record = PackageRecord.from_objects(
+                                package_cache_record
+                            )
+                            write_as_json_to_file(repodata_file, repodata_record)
+                            assert repodata_file.exists()
