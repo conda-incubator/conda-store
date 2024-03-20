95593e74260abf93befaf5cd6e2debdb7ca02eed
Patrik Kovacs <patrik.kovacs@catalysts.cc>
2024-03-20 09:56:58 +0200

    make conda_flags configurable

--- conda-store-server/conda_store_server/build.py
+++ conda-store-server/conda_store_server/build.py
@@ -176,6 +176,7 @@ def build_conda_environment(db: Session, conda_store, build):
                     build.specification.spec
                 ),
                 platforms=settings.conda_solve_platforms,
+                conda_flags=conda_store.conda_flags,
             )
 
             conda_store.storage.set(
@@ -273,6 +274,7 @@ def solve_conda_environment(db: Session, conda_store, solve: orm.Solve):
         conda_command=settings.conda_command,
         specification=schema.CondaSpecification.parse_obj(solve.specification.spec),
         platforms=[conda_utils.conda_platform()],
+        conda_flags=conda_store.conda_flags,
     )
     conda_lock_spec = context.result
 
