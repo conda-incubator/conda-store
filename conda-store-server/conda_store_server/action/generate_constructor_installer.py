faac59698402b4ebe790a3ff31e4044de1667630
Nikita Karetnikov <nikita@karetnikov.org>
2024-04-25 20:17:34 +0200

    Do not skip `noarch` platform

--- conda-store-server/conda_store_server/action/generate_constructor_installer.py
+++ conda-store-server/conda_store_server/action/generate_constructor_installer.py
@@ -69,7 +69,7 @@ def action_generate_constructor_installer(
             # platforms. constructor is cross-friendly, but we're currently
             # building only for the current architecture, see the comment in
             # get_installer_platform
-            if p.platform != get_installer_platform():
+            if p.platform not in ["noarch", get_installer_platform()]:
                 continue
             if p.manager == "pip":
                 pip_dependencies.append(f"{p.name}=={p.version}")
