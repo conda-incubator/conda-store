a3c37006dd548da5a375ef89c1950341733cb9e8
Tania Allard <taniar.allard@gmail.com>
2024-08-12 11:34:09 +0100

    :wrench: Change to dynamic version - conda-store-server

--- conda-store-server/conda_store_server/__init__.py
+++ conda-store-server/conda_store_server/__init__.py
@@ -5,10 +5,28 @@ import typing
 import platformdirs
 
 
-__version__ = "2024.6.1"
-
-
 CONDA_STORE_DIR = platformdirs.user_data_path(appname="conda-store")
+
+# Define the variable '__version__':
+try:
+    # If setuptools_scm is installed (e.g. in a development environment with
+    # an editable install), then use it to determine the version dynamically.
+    from setuptools_scm import get_version
+
+    # This will fail with LookupError if the package is not installed in
+    # editable mode or if Git is not installed.
+    __version__ = get_version(root="../..", relative_to=__file__)
+except (ImportError, LookupError):
+    # As a fallback, use the version that is hard-coded in the file.
+    try:
+        from conda_store_server._version import __version__  # noqa: F401
+    except ModuleNotFoundError:
+        # The user is probably trying to run this without having installed
+        # the package, so complain.
+        raise RuntimeError(
+            "conda-store-server is not correctly installed. "
+            "Please install it with pip."
+        )
 
 
 class BuildKey:
