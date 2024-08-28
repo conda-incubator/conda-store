96ff2e80abbb4c7290b6b11e3b195536267f8bc0
Tania Allard <taniar.allard@gmail.com>
2024-08-28 17:52:08 +0100

    Revert shebang

--- conda-store/tests/shebang.sh
+++ conda-store/tests/shebang.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env conda-store
-
 #! conda-store run filesystem/python-flask-env:1 -- python
 
 # filesystem/python-flask-env:1 is populated from tests/assets/environments/python-flask-env.yaml
