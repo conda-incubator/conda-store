2e92a377cd97f5ad51e0a56cd3b5fb62a7048bbf
Tania Allard <taniar.allard@gmail.com>
2024-09-10 15:43:53 +0100

    Update bundling hook

--- conda-store-server/hatch_build.py
+++ conda-store-server/hatch_build.py
@@ -1,3 +1,10 @@
+# Custom hook to build conda-store-server with a local or released version of conda-store-ui.
+# To build with a released version of conda-store-ui: make sure to set the CONDA_STORE_UI_VERSION to a valid release
+# version in npm.
+# Then run `hatch build` to build conda-store-server with the specified version of conda-store-ui.
+# To build with a local version of conda-store-ui: set the LOCAL_UI environment variable to the path of the local conda-store-ui directory.
+# For example: export LOCAL_UI=<local path for conda-store-ui>  && hatch build --clean
+
 import os
 import shutil
 import tarfile
@@ -8,22 +15,17 @@ from pathlib import Path
 from typing import Any, Dict, List
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
-from jinja2 import Environment, FileSystemLoader
 
 
 CONDA_STORE_UI_VERSION = "2024.3.1"
 CONDA_STORE_UI_URL = f"https://registry.npmjs.org/@conda-store/conda-store-ui/-/conda-store-ui-{CONDA_STORE_UI_VERSION}.tgz"
-CONDA_STORE_UI_FILES = [
-    "main.js",
-    "main.css",
-    "main.css.map",
-    "main.js.map",
-    "index.html",
-]
-UI_FILES_EXTENSIONS = ["*.js", "*.css", "*.map", "*.html"]
 
-SERVER_UI_ASSETS = "conda_store_server/_internal/server/static/conda-store-ui"
-SERVER_UI_TEMPLATES = "conda_store_server/_internal/server/static/templates"
+UI_FILES_EXTENSIONS = ["*.js", "*.css", "*.js.map", "*.css.map", "*.html"]
+
+SERVER_DIR = Path("conda_store_server/_internal/server")
+SERVER_UI_ASSETS = SERVER_DIR / "static/conda-store-ui"
+# FastAPI templates directory
+SERVER_UI_TEMPLATES = SERVER_DIR / "templates"
 
 
 # Note we do not modify the main.js file directly anymore. Instead we leverage
@@ -32,7 +34,8 @@ SERVER_UI_TEMPLATES = "conda_store_server/_internal/server/static/templates"
 # which is set up in conda-store-server/conda_store_server/_internal/server/templates/conda-store-ui.html
 class DownloadCondaStoreUIHook(BuildHookInterface):
     def clean(self, versions: List[str]) -> None:
-        """Quick utility method to remove any straggling ui files from previous versions
+        """Delete previously vendored UI files. This is called from:
+        `hatch clean` and `hatch build --clean`
 
         Args:
             versions (List[str]): a list of published versions in npm
@@ -42,9 +45,10 @@ class DownloadCondaStoreUIHook(BuildHookInterface):
         shutil.rmtree(server_build_static_assets, ignore_errors=True)
 
     def initialize(self, version: str, build_data: Dict[str, Any]) -> None:
-        """UI vendoring within conda-store-server, right now it downloads the
-        published UI, copies the distributed html, js and css files and
-        does some on the fly env vars injection.
+        """UI vendoring within conda-store-server. This can be used to build conda-store-server with a local or
+        released version of conda-store-ui (should be available in npm).
+        This hook ensures we have the UI files in the correct location for the server to serve them and updates the
+        HTML templates to point to the right paths for the UI assets.
 
         Args:
             version (str): ui version to vendor
@@ -65,6 +69,7 @@ class DownloadCondaStoreUIHook(BuildHookInterface):
                 raise FileNotFoundError(
                     f"Local UI directory {os.getenv('LOCAL_UI')} does not exist"
                 )
+        # build with a released version of conda-store-ui - this is what we do for conda-store-server releases
         else:
             print(f"Building with conda-store-ui version {CONDA_STORE_UI_VERSION}")
             self.get_ui_release(CONDA_STORE_UI_VERSION)
@@ -93,59 +98,34 @@ class DownloadCondaStoreUIHook(BuildHookInterface):
 
             source_directory = tmp_dir / "package/dist"
 
-            # server_build_static_assets = (Path(self.root)/ SERVER_UI_ASSETS)
-            # server_build_static_assets.mkdir(parents=True, exist_ok=True)
-
-            # print(f"Copying conda-store-ui {CONDA_STORE_UI_FILES}")
-            # for filename in CONDA_STORE_UI_FILES:
-            #     shutil.copy(
-            #         source_directory / filename,
-            #         server_build_static_assets / filename,
-            #     )
-
             self.copy_ui_files(source_directory)
 
     def copy_ui_files(self, source_directory: str) -> None:
-        """Copy a local version of conda-store-ui to the server's static assets
-        directory.
+        """Copy conda-store-ui files to the conda-server static assets
+        directory (SERVER_UI_ASSETS).
 
         Args:
-            local_ui_path (str): path to a local version of conda-store-ui
+            source_directory (str): path to the directory containing the UI files
         """
-        # source_directory = Path(local_ui_path)/ "dist"
+
         server_build_static_assets = Path(self.root) / SERVER_UI_ASSETS
         server_build_static_assets.mkdir(parents=True, exist_ok=True)
 
-        print(f"Copying conda-store-ui files from {source_directory}")
+        print(f"Copying conda-store-ui files from {source_directory} \n")
 
         try:
             for extension in UI_FILES_EXTENSIONS:
                 for file_path in source_directory.glob(extension):
                     target_path = server_build_static_assets / file_path.name
+                # in case the file already exists, remove it
                 if target_path.exists():
                     target_path.unlink()
                 shutil.copy(file_path, target_path)
 
-                # Print all files in the target directory after copying
-            print("Copied files:")
-            for file in server_build_static_assets.glob("*"):
-                print(file.name)
+            print(
+                f"Copied files: {[p.name for p in server_build_static_assets.glob("*")]}"
+            )
+
         except (IOError, OSError) as e:
             print(f"Error copying files: {e}")
             raise
-
-    def _update_ui_template():
-
-        # Render the Jinja template - note this is different from the FastAPI templates
-        template_dir = Path(SERVER_UI_ASSETS)
-        output_dir = Path(SERVER_UI_TEMPLATES)
-        env = Environment(loader=FileSystemLoader(template_dir))
-        template = env.get_template("conda-store-ui-template.html")
-
-        main_js_file = "static/conda-store-ui/main.8217000e38695a5bf780.js"
-        rendered_html = template.render(main_js_file=main_js_file)
-        output_path = os.path.join(output_dir, "conda-store-ui.html")
-        with open(output_path, "w") as f:
-            f.write(rendered_html)
-
-        print(f"Rendered HTML saved to {output_path}")
