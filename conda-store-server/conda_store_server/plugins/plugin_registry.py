d6763c99c1c964c119cb4e619b5c1793f31defe6
sophia <scastellarin@quansight.com>
2024-11-07 16:18:36 -0800

    Lint fixes

--- conda-store-server/conda_store_server/plugins/plugin_registry.py
+++ conda-store-server/conda_store_server/plugins/plugin_registry.py
@@ -10,7 +10,7 @@ class PluginRegistry():
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(PluginRegistry, cls).__new__(cls)
+            cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self):
