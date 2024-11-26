0fedec102e7e98a95953f5bca4dfabd724397fe0
sophia <scastellarin@quansight.com>
2024-11-26 15:23:13 -0800

    Bump dev version

--- conda-store-server/conda_store_server/__init__.py
+++ conda-store-server/conda_store_server/__init__.py
@@ -14,7 +14,7 @@ if typing.TYPE_CHECKING:
     from .app import CondaStore
 
 
-__version__ = "2024.11.2"
+__version__ = "2024.11.3-dev"
 
 
 CONDA_STORE_DIR = platformdirs.user_data_path(appname="conda-store")
