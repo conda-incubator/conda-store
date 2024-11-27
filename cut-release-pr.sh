f6b99f81fca82046d2b56b0ec3d9762b9d1423d3
sophia <scastellarin@quansight.com>
2024-11-27 14:30:49 -0800

    Extract hatch verfiy steps to a function

--- cut-release-pr.sh
+++ cut-release-pr.sh
@@ -5,12 +5,30 @@
 #   - github-activity https://github.com/choldgraf/github-activity
 #   - jq
 # - GITHUB_ACCESS_TOKEN env var set
+# - `conda-store-server-dev` conda env is activated
 
 function usage {
     echo -e "Usage: ./cut-release-pr.sh -r release_version -c conda_store_ui_version\n"
     exit 1
 }
 
+function build_and_check () {
+    # Runs a hatch build, twine check, and checks the version of the package
+    # Assumes that you are in the directory for the package
+    #
+    # args: 
+    #   CHECK_TARGET: the name of the package to check
+    CHECK_TARGET=$1
+
+    echo "${CHECK_TARGET}: Checking hatch versions..."
+    HV=$(hatch version)
+    if [[ "$HV" == "$RELEASE_VERSION" ]]; then
+        echo "${CHECK_TARGET} hatch version is matching the requested release version ${RELEASE_VERSION}"
+    else
+        echo "${CHECK_TARGET} hatch version does not match the requested release version. Something has gone wrong, exiting!"
+        exit 1
+    fi
+}
 
 while [[ $# -gt 0 ]]
 do
@@ -51,8 +69,8 @@ with
 "
 
 # prepare repo
-git checkout -b release-"$RELEASE_VERSION"
-git clean -fxdq
+# git checkout -b release-"$RELEASE_VERSION"
+# git clean -fxdq
 
 # bump versions
 sed -E -r -i "s/__version__ = .+/__version__ = \"$RELEASE_VERSION\"/g" conda-store-server/conda_store_server/__init__.py 
@@ -60,43 +78,30 @@ sed -E -r -i "s/__version__ = .+/__version__ = \"$RELEASE_VERSION\"/g" conda-sto
 sed -E -r -i "s/CONDA_STORE_UI_VERSION = .+/CONDA_STORE_UI_VERSION = \"$CONDA_STORE_UI_VERSION\"/g" conda-store-server/hatch_build.py      
 
 # create changelog
-LATEST_TAG=$(curl https://api.github.com/repos/conda-incubator/conda-store/releases | jq -r '.[0].tag_name')
-PYTHONWARNINGS="ignore" github-activity conda-incubator/conda-store --since $LATEST_TAG --heading-level=2 > tmp_changes.txt
-# remove first line of changes - it's always a message about which GH token is used
-sed -n -i '1!p' tmp_changes.txt
-# replace the header for the changes with the appropriate title
-sed -n -i '1!p' tmp_changes.txt
-sed -i "1s/^/## [$RELEASE_VERSION] - $CURRENT_DATE\n/" tmp_changes.txt
-# insert changes into changelog
-sed -i "/---/r tmp_changes.txt" CHANGELOG.md
-# clean up temp file
-rm tmp_changes.txt
+# LATEST_TAG=$(curl https://api.github.com/repos/conda-incubator/conda-store/releases | jq -r '.[0].tag_name')
+# PYTHONWARNINGS="ignore" github-activity conda-incubator/conda-store --since $LATEST_TAG --heading-level=2 > tmp_changes.txt
+# # remove first line of changes - it's always a message about which GH token is used
+# sed -n -i '1!p' tmp_changes.txt
+# # replace the header for the changes with the appropriate title
+# sed -n -i '1!p' tmp_changes.txt
+# sed -i "1s/^/## [$RELEASE_VERSION] - $CURRENT_DATE\n/" tmp_changes.txt
+# # insert changes into changelog
+# sed -i "/---/r tmp_changes.txt" CHANGELOG.md
+# # clean up temp file
+# rm tmp_changes.txt
 
-echo "Checking hatch versions..."
 cd conda-store-server
-HV=$(hatch version)
-if [[ "$HV" == "$RELEASE_VERSION" ]]; then
-    echo "conda-store-server hatch version is matching the requested release version ${RELEASE_VERSION}"
-else
-    echo "conda-store-server hatch version does not match the requested release version. Something has gone wrong, exiting!"
-    exit 1
-fi
+build_and_check conda-store-server
+cd ..
 
-cd ../conda-store
-HV=$(hatch version)
-if [[ "$HV" == "$RELEASE_VERSION" ]]; then
-    echo "conda-store hatch version is matching the requested release version ${RELEASE_VERSION}"
-else
-    echo "conda-store hatch version does not match the requested release version. Something has gone wrong, exiting!"
-    exit 1
-fi
-
+cd conda-store
+build_and_check conda-store
 cd ..
 
 
-# add files to git
-git add conda-store conda-store-server CHANGELOG.md
-git commit -m "REL - $RELEASE_VERSION"
+# # add files to git
+# git add conda-store conda-store-server CHANGELOG.md
+# git commit -m "REL - $RELEASE_VERSION"
 
 echo "
 Finished preparing the release!
