f97aa9b6c61eb7bfb7874c0fd43a788f723d8afd
sophia <scastellarin@quansight.com>
2024-11-27 14:42:55 -0800

    Add steps to build and check packages

--- cut-release-pr.sh
+++ cut-release-pr.sh
@@ -23,11 +23,24 @@ function build_and_check () {
     echo "${CHECK_TARGET}: Checking hatch versions..."
     HV=$(hatch version)
     if [[ "$HV" == "$RELEASE_VERSION" ]]; then
-        echo "${CHECK_TARGET} hatch version is matching the requested release version ${RELEASE_VERSION}"
+        echo "${CHECK_TARGET}: hatch version matches ${RELEASE_VERSION}"
     else
-        echo "${CHECK_TARGET} hatch version does not match the requested release version. Something has gone wrong, exiting!"
+        echo "${CHECK_TARGET}: hatch version does not match the requested release version. Something has gone wrong, exiting!"
         exit 1
     fi
+
+    echo "${CHECK_TARGET}: Running build and checking package..."
+    hatch build
+    rc=$(twine check dist/*)
+    if [[ $rc =~ "FAILED" ]]; then
+        echo "${CHECK_TARGET}: package is not in a good state, exiting!"
+        exit 1
+    else
+        echo "${CHECK_TARGET}: package is in a good state"
+    fi
+
+    echo "${CHECK_TARGET}: cleaning up..."
+    hatch clean
 }
 
 while [[ $# -gt 0 ]]
@@ -69,8 +82,8 @@ with
 "
 
 # prepare repo
-# git checkout -b release-"$RELEASE_VERSION"
-# git clean -fxdq
+git checkout -b release-"$RELEASE_VERSION"
+git clean -fxdq
 
 # bump versions
 sed -E -r -i "s/__version__ = .+/__version__ = \"$RELEASE_VERSION\"/g" conda-store-server/conda_store_server/__init__.py 
@@ -78,17 +91,17 @@ sed -E -r -i "s/__version__ = .+/__version__ = \"$RELEASE_VERSION\"/g" conda-sto
 sed -E -r -i "s/CONDA_STORE_UI_VERSION = .+/CONDA_STORE_UI_VERSION = \"$CONDA_STORE_UI_VERSION\"/g" conda-store-server/hatch_build.py      
 
 # create changelog
-# LATEST_TAG=$(curl https://api.github.com/repos/conda-incubator/conda-store/releases | jq -r '.[0].tag_name')
-# PYTHONWARNINGS="ignore" github-activity conda-incubator/conda-store --since $LATEST_TAG --heading-level=2 > tmp_changes.txt
-# # remove first line of changes - it's always a message about which GH token is used
-# sed -n -i '1!p' tmp_changes.txt
-# # replace the header for the changes with the appropriate title
-# sed -n -i '1!p' tmp_changes.txt
-# sed -i "1s/^/## [$RELEASE_VERSION] - $CURRENT_DATE\n/" tmp_changes.txt
-# # insert changes into changelog
-# sed -i "/---/r tmp_changes.txt" CHANGELOG.md
-# # clean up temp file
-# rm tmp_changes.txt
+LATEST_TAG=$(curl https://api.github.com/repos/conda-incubator/conda-store/releases | jq -r '.[0].tag_name')
+PYTHONWARNINGS="ignore" github-activity conda-incubator/conda-store --since $LATEST_TAG --heading-level=2 > tmp_changes.txt
+# remove first line of changes - it's always a message about which GH token is used
+sed -n -i '1!p' tmp_changes.txt
+# replace the header for the changes with the appropriate title
+sed -n -i '1!p' tmp_changes.txt
+sed -i "1s/^/## [$RELEASE_VERSION] - $CURRENT_DATE\n/" tmp_changes.txt
+# insert changes into changelog
+sed -i "/---/r tmp_changes.txt" CHANGELOG.md
+# clean up temp file
+rm tmp_changes.txt
 
 cd conda-store-server
 build_and_check conda-store-server
@@ -100,8 +113,8 @@ cd ..
 
 
 # # add files to git
-# git add conda-store conda-store-server CHANGELOG.md
-# git commit -m "REL - $RELEASE_VERSION"
+git add conda-store conda-store-server CHANGELOG.md
+git commit -m "REL - $RELEASE_VERSION"
 
 echo "
 Finished preparing the release!
