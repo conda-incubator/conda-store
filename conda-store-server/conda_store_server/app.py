eb8ceac35946639618d3ea156cd4a0172f9afa95
sophia <scastellarin@quansight.com>
2024-12-09 21:13:22 -0800

    Remove reference to registry enum

--- conda-store-server/conda_store_server/app.py
+++ conda-store-server/conda_store_server/app.py
@@ -280,9 +280,6 @@ class CondaStore(LoggingConfigurable):
             schema.BuildArtifactType.LOGS,
             schema.BuildArtifactType.LOCKFILE,
             schema.BuildArtifactType.YAML,
-            # no possible way to delete these artifacts
-            # in most container registries via api
-            schema.BuildArtifactType.CONTAINER_REGISTRY,
         ],
         help="artifacts to keep on build deletion",
         config=True,
