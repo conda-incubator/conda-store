1f4f01bbea355cede63df87a901d37cd5d03abee
sophia <scastellarin@quansight.com>
2024-12-17 15:26:27 -0800

    Don't ensure settings
    traitlets settings should not be persisted to the database
    instead load settings (in order of precedence):
      * read config from config file - deployment default
      * read config from settings db in order of specificity
        * global - set by admin
        * namespace
        * environment

--- conda-store-server/tests/conftest.py
+++ conda-store-server/tests/conftest.py
@@ -92,7 +92,6 @@ def conda_store_server(conda_store_config):
     dbutil.upgrade(_conda_store.config.database_url)
 
     with _conda_store.session_factory() as db:
-        _conda_store.ensure_settings(db)
         _conda_store.configuration(db).update_storage_metrics(
             db, _conda_store.config.store_directory
         )
@@ -177,7 +176,6 @@ def conda_store(conda_store_config):
     dbutil.upgrade(_conda_store.config.database_url)
 
     with _conda_store.session_factory() as db:
-        _conda_store.ensure_settings(db)
         _conda_store.configuration(db).update_storage_metrics(
             db, _conda_store.config.store_directory
         )
