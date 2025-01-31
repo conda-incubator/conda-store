c830d9c36b14fe70e51ee7ce324d93a6dca908aa
sophia <scastellarin@quansight.com>
2025-01-31 09:11:51 -0800

    REL - 2025.1.2-rc1

--- conda-store-server/conda_store_server/__init__.py
+++ conda-store-server/conda_store_server/__init__.py
@@ -14,7 +14,7 @@ if typing.TYPE_CHECKING:
     from .app import CondaStore
 
 
-__version__ = "2025.1.2-dev"
+__version__ = "2025.1.2-rc1"
 
 
 CONDA_STORE_DIR = platformdirs.user_data_path(appname="conda-store")
